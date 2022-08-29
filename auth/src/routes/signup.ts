25e6877b561cbbaa8172815a6fdf8dd863253497
Ihor Rohovets <freego555@gmail.com>
2022-08-29 10:30:29 +0300

    formatting signup.ts

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,16 +3,16 @@ import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', 
-[
-  body('email')
-    .isEmail()
-    .withMessage('Email must be valid'),
-  body('password')
-    .trim()
-    .isLength({ min: 4, max: 20 })
-    .withMessage('Password must be between 4 and 20 characters')
-], (req: Request, res: Response) => {
+router.post(
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 and 20 characters'),
+  ],
+  (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -24,6 +24,7 @@ router.post('/api/users/signup',
     console.log('Creating a user...');
 
     res.send({});
-});
+  }
+);
 
 export { router as signupRouter };
