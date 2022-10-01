dc4e00243355d7bed5254c9160fdb48e10bb41ff
Ihor Rohovets <freego555@gmail.com>
2022-10-01 14:40:53 +0300

    S09 - L189 Current User Handler

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,19 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!');
+router.get('/api/users/currentuser', (req: Request, res: Response) => {
+  if (!req.session?.jwt) {
+    return res.status(200).send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.status(200).send({ currentUser: payload });
+  } catch (err) {
+    res.status(200).send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
