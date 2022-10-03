83b5fbd5e0b171e9607e9d1a190c2736f7257325
Ihor Rohovets <freego555@gmail.com>
2022-10-03 23:25:05 +0300

    S09 - L192-193 Creating a Current User Middleware and Augmenting Type Definitions

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,19 +1,14 @@
 import express, { Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req: Request, res: Response) => {
-  if (!req.session?.jwt) {
-    return res.status(200).send({ currentUser: null });
+router.get(
+  '/api/users/currentuser',
+  currentUser,
+  (req: Request, res: Response) => {
+    res.status(200).send({ currentUser: req.currentUser || null });
   }
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    res.status(200).send({ currentUser: payload });
-  } catch (err) {
-    res.status(200).send({ currentUser: null });
-  }
-});
+);
 
 export { router as currentUserRouter };
