4fd63a46df40a9f83d642262445b59834c5d317d
Ihor Rohovets <freego555@gmail.com>
2022-10-12 14:15:43 +0300

    S11 - L238 On the Server or the Browser

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,18 +1,24 @@
 import axios from 'axios';
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  axios.get('/api/users/currentuser').catch((err) => {
-    console.log(err.message);
-  });
+  console.log('currentUser', currentUser);
+  // axios.get('/api/users/currentuser').catch((err) => {
+  //   console.log(err.message);
+  // });
 
   return <h1>Landing Page</h1>;
 };
 
-// LandingPage.getInitialProps = async () => {
-//   const response = await axios.get('/api/users/currentuser');
+LandingPage.getInitialProps = async () => {
+  if (typeof window === 'undefined') {
+    // we are on the server!
+    // requests should be made to http://ingress-nginx-controller.ingress-nginx.svc.cluster.local
+  } else {
+    // we are on the browser!
+    // requests can be made with a base url of ''
+  }
 
-//   return response.data;
-// };
+  return {};
+};
 
 export default LandingPage;
