3d1f60c8a7a12f8e9b00df35be3d8b3ec820dc9f
Ihor Rohovets <freego555@gmail.com>
2022-10-12 14:17:18 +0300

    S11 - L241 Specifying the Host

--- client/pages/index.js
+++ client/pages/index.js
@@ -13,9 +13,20 @@ LandingPage.getInitialProps = async () => {
   if (typeof window === 'undefined') {
     // we are on the server!
     // requests should be made to http://ingress-nginx-controller.ingress-nginx.svc.cluster.local
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: {
+          Host: 'ticketing.dev',
+        },
+      }
+    );
+    return data;
   } else {
     // we are on the browser!
     // requests can be made with a base url of ''
+    const { data } = await axios.get('/api/users/currentuser');
+    return data;
   }
 
   return {};
