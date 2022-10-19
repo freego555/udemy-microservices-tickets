cfd5b6370ee867926f774c0f299b63092f9dd312
Ihor Rohovets <freego555@gmail.com>
2022-10-19 20:47:03 +0300

    S13 - L290 Final Update Changes

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -46,6 +46,64 @@ it('returns a 401 if the user does not own the ticket', async () => {
     .expect(401);
 });
 
-it('returns a 400 if the user provides an invalid title or price', async () => {});
+it('returns a 400 if the user provides an invalid title or price', async () => {
+  const cookie = global.signin();
 
-it('updates the ticket provided valid inputs', async () => {});
+  const response = await request(app)
+    .post('/api/tickets/')
+    .set('Cookie', cookie)
+    .send({
+      title: 'Title',
+      price: 10,
+    });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: '',
+      price: 20,
+    })
+    .expect(400);
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'New title',
+      price: -10,
+    })
+    .expect(400);
+});
+
+it('updates the ticket provided valid inputs', async () => {
+  const cookie = global.signin();
+
+  const response = await request(app)
+    .post('/api/tickets/')
+    .set('Cookie', cookie)
+    .send({
+      title: 'Title',
+      price: 10,
+    });
+
+  const newTitle = 'New Title';
+  const newPrice = 20;
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: newTitle,
+      price: newPrice,
+    })
+    .expect(200);
+
+  const responseTicket = await request(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .send()
+    .expect(200);
+
+  expect(responseTicket.body.title).toEqual(newTitle);
+  expect(responseTicket.body.price).toEqual(newPrice);
+});
