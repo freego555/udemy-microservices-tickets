3fb449cb959027423837a827b8f63bae94acc929
Ihor Rohovets <freego555@gmail.com>
2022-10-19 20:16:53 +0300

    S13 - L289 Permission Checking

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -15,9 +15,15 @@ router.put(
   requireAuth,
   async (req: Request, res: Response) => {
     const ticket = await Ticket.findById(req.params.id);
+
     if (!ticket) {
       throw new NotFoundError();
     }
+
+    if (ticket.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
     res.send(ticket);
   }
 );
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -37,7 +37,7 @@ afterAll(async () => {
 global.signin = () => {
   // Build a JWT payload. { id, email }
   const payload = {
-    id: 'd3a422h260',
+    id: new mongoose.Types.ObjectId().toHexString(),
     email: 'test@test.com',
   };
 
