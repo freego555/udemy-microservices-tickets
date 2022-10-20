4cfd0690f9a71ef45d81c716e9ba193a7e60e4c1
Ihor Rohovets <freego555@gmail.com>
2022-10-20 20:58:50 +0300

    S14 - L315 Durable Subscriptions

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -18,8 +18,13 @@ stan.on('connect', () => {
   const options = stan
     .subscriptionOptions()
     .setManualAckMode(true)
-    .setDeliverAllAvailable();
-  const subscription = stan.subscribe('ticket:created', options);
+    .setDeliverAllAvailable()
+    .setDurableName('orders-service');
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'queue-group-name',
+    options
+  );
 
   subscription.on('message', (msg: Message) => {
     const data = msg.getData();
